332364425e0cc2a71294300cc2d6f8a9c3a6a661
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-09-26 11:31:58 +0200

    Finished server-side authentication

--- authentication_server/controllers/authentication.js
+++ authentication_server/controllers/authentication.js
@@ -1,8 +1,25 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user) });
+};
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide an email and a password " });
+  }
 
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) {
@@ -21,7 +38,7 @@ exports.signup = function(req, res, next) {
         return next(err);
       }
 
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
