09167ab4bbecf6322835b22d87e5d8e574b5cb63
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-10-01 16:08:20 +0200

    HOC Finished

--- higher_order_components/src/components/app.js
+++ higher_order_components/src/components/app.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class App extends Component {
+import Header from "./header";
+
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps)(App);
